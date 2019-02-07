da53d7da0db26a7702e490b7a36df2605ebe00a2
JPDF <patrik.forsslund@gmail.com>
2019-02-07 17:17:07 +0100

    Reworked languages.json to fit kattis submission
    Languages.json keys now reflects the languages from kattis supported
    languages and template names.
    New property:
     - tags: shortcut for identifying languages when using commands

---
+++ utils/templates/C++.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Problem here
+    return 0;
+}
--- utils/templates/c_template.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int count;
-    scanf("%d", &count);
-
-    printf("");
-    return 0;
-}
