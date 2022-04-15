de5ee0b4e6349ac1d1ed30abf41fe1448c980867
Patrik Forsslund <patrik.forsslund@gmail.com>
2022-04-15 15:23:42 +0200

    Add search for language if not found directly

--- utils/settings.js
+++ utils/settings.js
@@ -17,8 +17,21 @@ settings.language = process.argv[3] || storage.get('language')
 if (settings.language == null)
 	log.error('Language input not found')
 
-if (typeof languages[settings.language] === 'undefined')
-	log.error(`Selected language '${settings.language}' does not exist`)
+if (typeof languages[settings.language] === 'undefined') {
+	// Try to find the language
+	const foundKey = Object.keys(languages).find(key => {
+		return (
+			key.toLowerCase().replace(' ', '') === settings.language.toLowerCase() ||
+			(languages[key].hasOwnProperty('alias') &&
+				languages[key].alias.find(a => a == settings.language))
+		)
+	})
+	if (typeof foundKey === 'undefined') {
+		log.error(`Selected language '${settings.language}' does not exist`)
+	} else {
+		settings.language = foundKey
+	}
+}
 
 Object.keys(languages[settings.language]).forEach(key => {
 	settings[key] = languages[settings.language][key]
